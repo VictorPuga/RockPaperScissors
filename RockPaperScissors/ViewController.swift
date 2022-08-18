69165dfec36ea5240a36a1b0830ecf6c0c3dfcf9
VictorPuga <39507381+VictorPuga@users.noreply.github.com>
2022-08-18 15:46:49 -0600

    first commit

---
+++ RockPaperScissors/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RockPaperScissors
+//
+//  Created by Víctor Manuel Puga Ruiz on 18/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
